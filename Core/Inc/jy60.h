3ead208365314260109f37166372170df039d791
JakkuSakura <qjk2001@gmail.com>
2024-11-18 02:29:53 +0800

    feat: parse jy60 messages

--- Core/Inc/jy60.h
+++ Core/Inc/jy60.h
@@ -25,7 +25,7 @@ char IICMODECMD[3] = {0XFF, 0XAA, 0X62};
 // ay =((AyH<<8)|AyL)/32768*16g(g 为重力加速度，可取 9.8m/s2 )
 // az =((AzH<<8)|AzL)/32768*16g(g 为重力加速度，可取 9.8m/s2 )
 
-float get_acceleration(uint8_t AxH, uint8_t AxL) {
+static inline float get_acceleration(uint8_t AxH, uint8_t AxL) {
     return ((((int16_t) AxH << 8) | (int16_t) AxL) / 32768 * 16) * 9.8;
 }
 
@@ -33,7 +33,7 @@ float get_acceleration(uint8_t AxH, uint8_t AxL) {
 // wy =((wyH<<8)|wyL)/32768*2000(°/s)
 // wz=((wzH<<8)|wzL)/32768*2000(°/s)
 
-float get_angular_velocity(uint8_t wxH, uint8_t wxL) {
+static inline float get_angular_velocity(uint8_t wxH, uint8_t wxL) {
     return ((((int16_t) wxH << 8) | (int16_t) wxL) / 32768 * 2000);
 }
 
@@ -41,45 +41,94 @@ float get_angular_velocity(uint8_t wxH, uint8_t wxL) {
 // 俯仰角（y 轴）Pitch=((PitchH<<8)|PitchL)/32768*180(°)
 // 偏航角（z 轴）Yaw=((YawH<<8)|YawL)/32768*180(°)
 
-float get_angle(uint8_t RollH, uint8_t RollL) {
+static inline float get_angle(uint8_t RollH, uint8_t RollL) {
     return (((int16_t) RollH << 8) | (int16_t) RollL) / 32768 * 180.0;
 }
 
 //用串口2给JY模块发送指令
-void sendcmd(char cmd[]) {
+static inline void sendcmd(char cmd[]) {
     HAL_UART_Transmit(&huart3, (uint8_t *) cmd, 3, 1000);
 }
 
-void init_jy60() {
-    printf("正在进行加速度校准\r\n");
+static inline void init_jy60() {
+//    printf("正在进行加速度校准\r\n");
     sendcmd(ACCCMD);//等待模块内部自动校准好，模块内部会自动计算需要一定的时间
-    printf("加速度校准完成\r\n");
+//    printf("加速度校准完成\r\n");
     HAL_Delay(100);
-    printf("进行Z轴角度清零\r\n");
+//    printf("进行Z轴角度清零\r\n");
     sendcmd(YAWCMD);
-    printf("Z轴角度清零完成\r\n");
+//    printf("Z轴角度清零完成\r\n");
+    // vertical install
+    sendcmd(IICMODECMD);
 }
 
+static inline void process_packet(uint8_t *packet, StateRotation *rotation, StateAngularVelocity *ang_vec) {
+    uint8_t type = packet[1];
+    switch (type) {
+        case 0x51: {
+            // acceleration
+            break;
+        }
+        case 0x52: {
+            // angular velocity
+            uint8_t wxL = packet[2];
+            uint8_t wxH = packet[3];
+            uint8_t wyL = packet[4];
+            uint8_t wyH = packet[5];
+            uint8_t wzL = packet[6];
+            uint8_t wzH = packet[7];
+            ang_vec->x = get_angular_velocity(wxH, wxL);
+            ang_vec->y = get_angular_velocity(wyH, wyL);
+            ang_vec->z = get_angular_velocity(wzH, wzL);
+            break;
+        }
+        case 0x53: {
+            // angle
+            uint8_t rollL = packet[2];
+            uint8_t rollH = packet[3];
+            uint8_t pitchL = packet[4];
+            uint8_t pitchH = packet[5];
+            uint8_t yawL = packet[6];
+            uint8_t yawH = packet[7];
+            rotation->x = get_angle(rollH, rollL);
+            rotation->y = get_angle(pitchH, pitchL);
+            rotation->z = get_angle(yawH, yawL);
+
+            break;
+        }
+    }
+}
+
+#define BUFFER_SIZE 11
+#define MAX_READ_SIZE 32 // Maximum bytes you can read at once
+#define TOTAL_BUFFER_SIZE (BUFFER_SIZE + MAX_READ_SIZE)
 
 static inline void parse_jy60(uint8_t *data, uint16_t len, StateRotation *rotation, StateAngularVelocity *ang_vec) {
+    static uint8_t buffer[TOTAL_BUFFER_SIZE];
+    static uint8_t index = 0;
+    // Append new data to the buffer if there's enough space
+    if (index + len > TOTAL_BUFFER_SIZE) {
+        // Handle overflow by discarding excess data
+        index = 0; // Optionally reset the index here
+    }
 
-    // ucRxBuffer[ucRxCnt++]=ucData;	//将收到的数据存入缓冲区中
-    //	if (ucRxBuffer[0]!=0x55) //数据头不对，则重新开始寻找0x55数据头
-    //	{
-    //		ucRxCnt=0;
-    //		return;
-    //	}
-    //	if (ucRxCnt<11) {return;}//数据不满11个，则返回
-    //	else
-    //	{
-    //		switch(ucRxBuffer[1])//判断数据是哪种数据，然后将其拷贝到对应的结构体中，有些数据包需要通过上位机打开对应的输出后，才能接收到这个数据包的数据
-    //		{
-    //			//memcpy为编译器自带的内存拷贝函数，需引用"string.h"，将接收缓冲区的字符拷贝到数据结构体里面，从而实现数据的解析。
-    //			case 0x51:	memcpy(&stcAcc,&ucRxBuffer[2],8);break;
-    //			case 0x52:	memcpy(&stcGyro,&ucRxBuffer[2],8);break;
-    //			case 0x53:	memcpy(&stcAngle,&ucRxBuffer[2],8);break;
-    //
-    //		}
-    //		ucRxCnt=0;//清空缓存区
-    //	}
+    // Append new data
+    memcpy(&buffer[index], data, len);
+    index += len;
+
+    // Process packets
+    for (uint8_t i = 0; i <= index - BUFFER_SIZE;) {
+        if (buffer[i] == 0x55) {
+            // Found a start of a packet
+            if (i + BUFFER_SIZE <= index) {
+                // Process the complete packet
+                process_packet(&buffer[i], rotation, ang_vec);
+                i += BUFFER_SIZE;
+            } else {
+                memmove(&buffer[0], &buffer[i], index - i);
+                index -= i;
+                break;
+            }
+        }
+    }
 }
